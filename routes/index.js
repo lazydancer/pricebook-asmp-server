c63ed0a0e88df0f405f00d10fe91e38f58450cc3
lazydancer <jrpucula@gmail.com>
2025-09-25 11:19:28 -0400

    Add /v1/mod-version endpoint returning static min version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ const registerWaystoneRoutes = require('./waystones');
 const registerChunkRoutes = require('./chunks');
 const registerItemRoutes = require('./items');
 
+const MIN_MOD_VERSION = '1.0.0';
+
 const registerRoutes = (app, ctx) => {
+  app.get('/v1/mod-version', (_req, res) => {
+    res.json({ min_version: MIN_MOD_VERSION });
+  });
+
   registerScanRoutes(app, ctx);
   registerWaystoneRoutes(app, ctx);
   registerChunkRoutes(app, ctx);
